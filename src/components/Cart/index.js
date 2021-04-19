21c396929d1466b0d5c023f7527e96903739730c
Lucas <dexterfail@hotmail.com>
2021-04-19 10:58:54 -0300

    Criacao das funcionalidades do carrinho

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,22 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+
 import {
     CartArea,
     CartHeader,
     CartIcon,
     CartText,
     CartBody,
+    ProductsArea,
+    ProductItem,
+    ProductPhoto,
+    ProductInfoArea,
+    ProductName,
+    ProductPrice,
+    ProductQuantityArea,
+    ProductQtIcon,
+    ProductQtText
 } from "./styled";
 
 export default () => {
- return (
-    <CartArea>
-        <CartHeader>
-            <CartIcon src="/assets/cart.png" />
-            <CartText>Meu Carrinho (x)</CartText>
-        </CartHeader>
-        <CartBody>
+    const dispatch = useDispatch();
+    const products = useSelector(state => state.cart.products);
 
-        </CartBody>
-    </CartArea>
- );
+    const [show, setShow] = useState(true);
+
+    const handleCartClick = () => {
+        setShow(!show);
+    }
+
+    const handleProductChange = (key, type) => {
+        dispatch({
+            type: "CHANGE_PRODUCT",
+            payload:{key, type}
+        });
+    }
+
+    return (
+        <CartArea>
+            <CartHeader onClick={handleCartClick}>
+                <CartIcon src="/assets/cart.png" />
+                <CartText>Meu Carrinho ({products.length})</CartText>
+                {show &&
+                    <CartIcon src="./assets/down.png" />
+                }
+            </CartHeader>
+            <CartBody show={show}>
+                <ProductsArea>
+                    {products.map((item, index)=>(
+                        <ProductItem key={index}>
+                          <ProductPhoto src={item.image}/>
+                          <ProductInfoArea>
+                                <ProductName>{item.name}</ProductName>
+                                <ProductPrice>R$ {item.price.toFixed(2)}</ProductPrice>
+                          </ProductInfoArea>
+                          <ProductQuantityArea>
+                                <ProductQtIcon 
+                                    src="/assets/minus.png" 
+                                    onClick={()=> handleProductChange(index, "-")}
+                                />
+                                <ProductQtText>{item.qt}</ProductQtText>
+                                <ProductQtIcon 
+                                    src="/assets/plus.png" 
+                                    onClick={()=> handleProductChange(index, "+")}
+                                />
+                          </ProductQuantityArea>
+                        </ProductItem>
+                    ))}
+                  
+                </ProductsArea>
+            </CartBody>
+        </CartArea>
+    );
 }
--- src/reducers/CartReducer.js
+++ src/reducers/CartReducer.js
@@ -6,9 +6,10 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
+    let products = [...state.products];
+
     switch(action.type) {
         case "ADD_PRODUCT":
-            let products = [...state.products];
             let id = action.payload.data.id;
 
             let index = products.findIndex(item => item.id === id);
@@ -21,11 +22,26 @@ export default (state = initialState, action) => {
                 });
             }
 
-            console.log(products);
+            return{...state,products};
+        break;
+        case "CHANGE_PRODUCT":
+            if(products[action.payload.key]) {
+                switch(action.payload.type) {
+                    case "-":
+                        products[action.payload.key].qt--;
+
+                        if(products[action.payload.key].qt <= 0) {
+                            products = products.filter((item,index)=>index != action.payload.key);
+                        }
+                    break;
+                    case "+":
+                        products[action.payload.key].qt++;
+                    break;
+                }
+            }
 
             return{...state,products};
         break;
-
         // case "SET_TOKEN":
         //     return {...state, token: action.payload.token};
         // case 'SET_NAME':
